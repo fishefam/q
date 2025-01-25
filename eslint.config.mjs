99c6e9b726346818f5f5c19d01ce08a8c5c40ea3
Truong Nguyen <mnguyen@evenica.com>
2025-01-25 18:36:21 -0500

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,48 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import perfectionist from 'eslint-plugin-perfectionist'
+import tailwindcss from 'eslint-plugin-tailwindcss'
+import unicorn from 'eslint-plugin-unicorn'
+import unusedImports from 'eslint-plugin-unused-imports'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+/** @type {import('eslint').Linter.Config} */
+const config = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...tailwindcss.configs['flat/recommended'],
+  perfectionist.configs['recommended-alphabetical'],
+  unicorn.configs['flat/recommended'],
+  {
+    ignores: ['src/shared/shadcn'],
+  },
+  {
+    rules: {
+      'react/self-closing-comp': 'error',
+    },
+  },
+  {
+    plugins: { 'unused-imports': unusedImports },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          vars: 'all',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+]
 
-export default eslintConfig;
+export default config
---
+++ index.d.ts
@@ -0,0 +1,35 @@
+import type React from 'react'
+
+declare global {
+  type HOC = (element: React.JSX.Element) => React.JSX.Element
+
+  type HTMLAttributes = React.HTMLAttributes<HTMLElement> &
+    React.ImgHTMLAttributes<HTMLImageElement> &
+    Record<`data-${string}`, string>
+
+  type HTMLTagName = keyof HTMLElementTagNameMap
+
+  type LayoutProperties<P = UnknownRecord> = { children: ReactNode } & {
+    params: Promise<P>
+  }
+
+  type PageProperties<P = UnknownRecord, S = UnknownRecord> = {
+    params: Promise<P>
+    searchParams: Promise<Partial<S>>
+  }
+
+  type Properties<T = UnknownRecord> = {
+    children?: ReactNode
+    className?: string
+  } & T
+
+  type ReactNode = React.ReactNode
+
+  type SetState<T> = React.Dispatch<React.SetStateAction<T>>
+
+  type StringRecord = Record<string, string>
+
+  type UnknownRecord = Record<string, unknown>
+
+  type UseState<T> = [T, SetState<T>]
+}
